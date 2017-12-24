adb62d389ed066f99e28e36288242861b6c6c668
Jay Chan <jay@eventbrite.com>
2017-12-24 01:45:39 -0800

    game state object layer

--- game.py
+++ game.py
@@ -1,70 +1,81 @@
+from collections import namedtuple
 import json
 import random
 from redis import StrictRedis
+from string import digits, ascii_uppercase
+import time
 
 import settings
 
 
-STATE_LOBBY = 10
-STATE_STARTED = 20
-STATE_ENDGAME = 30
-STATE_ENDED = 40
+PHASE_LOBBY = 1
+PHASE_STARTED = 2
+PHASE_ENDGAME = 3
+PHASE_ENDED = 4
 
-REDIS_GAME_FORMAT = 'game:%s'
+NAME_CHARS = ascii_uppercase
 
-NAME_CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-''' ****REDIS SCHEMA****
-
-game:ABCDEF => JSON({
-    state: 10,
-    step: 0,
-    phase_elapse: 24,
-    bag: 'AAABBCC...',
-    table: 'JHYREP...',
-    players: {
-        '<nonce>': {
-            name: 'Albert',
-            words: ['FOVIA', ...],
-        }
-    },
-})
-
+State = namedtuple('State', ['phase', 'step', 'start_ts', 'bag', 'table', 'players', 'nonce'])
+''' **** SCHEMA ****
+    phase: phase number
+    step: vector clock
+    start_ts: start_ts of this phase
+    bag: list of letters
+    table: list of words
+    players: list of (name, words) tuples
+    nonce: dict mapping nonce key to player number
 '''
-
-class State(object):
-    __slots__ = [
-        'state',
-        ste
 
 
 class Game(object):
 
-    def __init__(self):
-        #self.state = STATE_LOBBY
-        #self.bag = settings.SCRABBLE_LETTERS
-
-        self.name = None
+    def __init__(self, name=None):
+        self.name = name
+        self.state = None
         self.redis = StrictRedis(**settings.REDIS)
+
+    def reset(self):
+        bag = list(settings.SCRABBLE_LETTERS)
+        random.shuffle(bag)
+        self.state = State(
+            phase=PHASE_LOBBY,
+            step=0,
+            start_ts=time.time(),
+            bag=''.join(bag),
+            table=[],
+            players=[],
+            nonce={},
+        )
 
     @classmethod
     def create(cls):
         game = cls()
-        while True:
-            name = ''.join(random.choice(NAME_CHARS) for i in xrange(6))
-            if game.redis.setnx(REDIS_GAME_FORMAT % (name,), ''):
-                break
-
-        game.redis.expire(REDIS_GAME_FORMAT % (name,), settings.GAME_TTL)
-        game.name = name
+        game.reset()
+        success = False
+        while not success:
+            game.name = ''.join(random.choice(NAME_CHARS) for i in xrange(5))
+            success = game.store(initial=True)
         return game
 
-    def load(self):
-        pass
+    @property
+    def redis_key(self):
+        return 'game:%s' % (self.name,)
 
-    def store(self):
-        pass
+    def store(self, initial=False):
+        serialized = json.dumps(self.state)
+        success = self.redis.set(self.redis_key, serialized, nx=initial)
+        if success and initial:
+            self.redis.expire(self.redis_key, settings.GAME_TTL)
+        return success
+
+    def load(self):
+        serialized = self.redis.get(self.redis_key)
+        self.state = State(*json.loads(serialized))
 
 if __name__ == '__main__':
     game = Game.create()
     print game.name
+
+    game.load()
+    print game.state
