918910978eb772b16f7492c5b2792b1b149138a8
Jay Chan <jay@eventbrite.com>
2017-12-26 16:33:21 -0800

    moving python source to src dir

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,19 @@
+import os
+
+BASE_DIR = os.path.join(os.path.dirname(__file__), '..')
+
+WORD_LIST = os.path.join(BASE_DIR, 'data/twl.txt')
+
 SCRABBLE_LETTERS = (
     'AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLL'
     'LLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ'
 )
+
+REDIS = dict(
+    host='localhost',
+    port=6379,
+    db=0,
+)
+
+GAME_TTL = 60 * 60
+LOCK_TTL = 5
