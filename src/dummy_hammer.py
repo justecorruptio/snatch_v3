79b413be59d27e17fef22984232fdea0090047a1
Jay Chan <crookedjustice@hotmail.com>
2017-12-29 09:06:52 -0800

    updating hammer script

--- src/dummy_hammer.py
+++ src/dummy_hammer.py
@@ -6,12 +6,15 @@ import time
 BASE_URL = 'http://localhost/snatch_api/game'
 
 
-r = requests.post(BASE_URL)
-name = r.json()['name']
-handle = 'Jay'
-r = requests.post(BASE_URL + '/' + name + '/join', json={'handle': handle})
-nonce = r.json()['nonce']
+while True:
+    r = requests.post(BASE_URL)
+    name = r.json()['name']
+    handle = 'Jay'
+    r = requests.post(BASE_URL + '/' + name + '/join', json={'handle': handle})
+    nonce = r.json()['nonce']
 
-r = requests.post(BASE_URL + '/' + name + '/addBot', json={'nonce': nonce, 'level': 5})
+    r = requests.post(BASE_URL + '/' + name + '/addBot', json={'nonce': nonce, 'level': 5})
 
-requests.post(BASE_URL + '/' + name + '/start', json={'nonce': nonce})
+    requests.post(BASE_URL + '/' + name + '/start', json={'nonce': nonce})
+
+    time.sleep(.1)
